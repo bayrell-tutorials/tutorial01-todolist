96dbfd273fc044aea7d521999c8f36f6feee28a1
Ildar Bikmamatov <vistoyn@gmail.com>
2018-02-05 22:25:53 +0600

    add detect consul ip by interface gateway

--- mongodb/root/consul.sh
+++ mongodb/root/consul.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-CONSUL_IP="172.17.0.1:8500"
+INTERFACE="eth0"
+
 
 while [ 1 ]; do
-
+	
+	# Get Consul IP
+	GATEWAY=`route |grep ${INTERFACE} | grep default | awk '{print $2}'`
+	CONSUL_IP="${GATEWAY}:8500"
+	
 	# Register RabbitMQ service in consul
-	IP=`ifconfig eth0 | grep inet |  awk '{print $2}' | sed -n 1p`
-	DATA="{\"ID\": \"mongodb_id01\",\"Name\": \"mongodb\", \"Address\": \"${IP}\"}"
+	IP=`ifconfig ${INTERFACE} | grep inet\ addr | awk '{print $2}' | cut -d ':' -f 2`
+	DATA="{\"ID\": \"mongodb_id01\",\"Name\": \"mongodb\", \"Address\": \"${IP}\", \"Port\": 27017}"
 	curl -H "Content-Type: application/json" -X PUT -d "${DATA}" http://${CONSUL_IP}/v1/agent/service/register
 
 	sleep 60
--- rabbitmq/root/consul.sh
+++ rabbitmq/root/consul.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-CONSUL_IP="172.17.0.1:8500"
+INTERFACE="eth0"
+
 
 while [ 1 ]; do
-
+	
+	# Get Consul IP
+	GATEWAY=`route |grep ${INTERFACE} | grep default | awk '{print $2}'`
+	CONSUL_IP="${GATEWAY}:8500"
+	
 	# Register RabbitMQ service in consul
-	IP=`ifconfig eth0 | grep inet |  awk '{print $2}' | sed -n 1p`
-	DATA="{\"ID\": \"rabbitmq_id01\",\"Name\": \"rabbitmq\", \"Address\": \"${IP}\"}"
+	IP=`ifconfig ${INTERFACE} | grep inet |  awk '{print $2}' | sed -n 1p`
+	DATA="{\"ID\": \"rabbitmq_id01\",\"Name\": \"rabbitmq\", \"Address\": \"${IP}\", \"Port\": 5672}"
 	curl -H "Content-Type: application/json" -X PUT -d "${DATA}" http://${CONSUL_IP}/v1/agent/service/register
 
 	sleep 60
